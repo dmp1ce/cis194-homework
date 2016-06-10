39741b9dd3d96bba185211c4146a4551bd93a39a
David <daveparrish@gmail.com>
2016-06-10 19:04:01 -0400

    Not the smallest, but working versions of all 3 functions

--- homework3/golf/src/Golf.hs
+++ homework3/golf/src/Golf.hs
@@ -2,6 +2,10 @@ module Golf
   (
     skips
   , localMaxima
+  , histogram
+  , sumOccurrences
+  , boolToHistogramPoint
+  , histogramLevel
   ) where
 
 -- I realized that splitAt in Prelude was close to what I needed so
@@ -30,3 +34,34 @@ localMaxima (x:y:z:ss)
   | x < y && z < y  = [y] ++ localMaxima (y:z:ss)
   | otherwise       = localMaxima (y:z:ss)
 localMaxima _       = []
+
+-- For the length of the integer list,
+-- Look for occurences equal to thatamount
+-- If it exists then print that line with those numbers occuring
+histogram :: [Integer] -> String
+histogram is
+  | is == []  = footer
+  | otherwise = (histogram' (toInteger maxOccurrences)) ++  footer
+  where
+    maxOccurrences = maximum (map (sumOccurrences is) [0..9])
+    footer = "=========\n0123456789\n"
+    histogram' :: Integer -> String
+    histogram' 0 = ""
+    histogram' x = (histogramLevel is x)  ++ (histogram' (x-1))
+
+histogramLevel  :: [Integer]  -- List to work on
+                -> Integer    -- Level of histogram to print
+                -> String     -- The pinted out level
+histogramLevel is level =  map boolToHistogramPoint (map (level<=) (map (sumOccurrences is) [0..9])) ++ "\n"
+
+boolToHistogramPoint :: Bool -> Char
+boolToHistogramPoint True  = '*'
+boolToHistogramPoint False = ' '
+
+sumOccurrences  :: [Integer]  -- List to find occurences
+                -> Integer        -- Integer to find
+                -> Integer        -- Number of occurences
+sumOccurrences [] _         = 0
+sumOccurrences (x:xs) comp
+  | x == comp               = 1 + (sumOccurrences xs comp)
+  | otherwise               = sumOccurrences xs comp
--- homework3/golf/test/GolfSpec.hs
+++ homework3/golf/test/GolfSpec.hs
@@ -3,7 +3,7 @@ module GolfSpec where
 import Test.Hspec
 --import Test.Hspec.QuickCheck
 
-import Golf (skips, localMaxima)
+import Golf (skips, localMaxima, histogram)
 
 main :: IO ()
 main = hspec spec
@@ -30,3 +30,16 @@ spec =
         localMaxima  [1,2,3,4,5] `shouldBe` []
       it "Example 4" $ do
         localMaxima [] `shouldBe` []
+    describe "histogram functionality" $ do
+      it "Example 1" $ do
+        histogram [1,1,1,5] `shouldBe`
+          " *        \n *        \n *   *    \n=========\n0123456789\n"
+      it "Example 2" $ do
+        histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe`
+          "    *     \n    *     \n    * *   \n ******  *\n=========\n0123456789\n"
+      it "Example 3" $ do
+        histogram [3,5] `shouldBe`
+          "   * *    \n=========\n0123456789\n"
+      it "Emtpy histogram" $ do
+        histogram [] `shouldBe`
+          "=========\n0123456789\n"
