80af9b1a23bd20a1f853701f8cc73e5a26aa34b0
David <daveparrish@gmail.com>
2016-06-15 22:02:32 -0400

    Add xor tests

--- homework4/xor/src/Lib.hs
+++ homework4/xor/src/Lib.hs
@@ -12,4 +12,4 @@ ourAdd :: Int  -- ^ left
 ourAdd x y = x + y
 
 xor :: [Bool] -> Bool
-xor = even . foldr (\x acc -> if x == True then (acc + 1::Integer) else acc) 0
+xor = odd . foldr (\x acc -> if x == True then (acc + 1::Integer) else acc) 0
--- homework4/xor/test/LibSpec.hs
+++ homework4/xor/test/LibSpec.hs
@@ -3,7 +3,7 @@ module LibSpec where
 import Test.Hspec
 import Test.Hspec.QuickCheck
 
-import Lib (ourAdd)
+import Lib (ourAdd, xor)
 
 main :: IO ()
 main = hspec spec
@@ -15,3 +15,8 @@ spec =
       True `shouldBe` True
     prop "ourAdd is commutative" $ \x y ->
       ourAdd x y `shouldBe` ourAdd y x
+    describe "xor" $ do
+      it "example 1" $ do
+        xor [False, True, False] `shouldBe` True
+      it "example 2" $ do
+        xor  [False, True, False, False, True] `shouldBe` False
