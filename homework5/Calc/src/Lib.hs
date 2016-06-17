91738521bb04f5a64bdc512caccc8b51bafb207f
David <daveparrish@gmail.com>
2016-06-17 15:47:37 -0400

    Add exercise 2 of homework 5

--- homework5/Calc/src/Lib.hs
+++ homework5/Calc/src/Lib.hs
@@ -2,9 +2,12 @@
 module Lib
     (
       eval
+    , evalStr
     ) where
 
 import ExprT
+import Data.Maybe (isNothing)
+import Parser (parseExp)
 
 --data ExprT = Lit Integer
 --           | Add ExprT ExprT
@@ -16,3 +19,11 @@ eval :: ExprT -> Integer
 eval (Mul e1 e2)  = (eval e1) * (eval e2)
 eval (Add e1 e2)  = (eval e1) + (eval e2)
 eval (Lit i)      = i
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr s = evalMaybe $ parseExp Lit Add Mul s
+  where 
+    evalMaybe :: Maybe ExprT -> Maybe Integer
+    evalMaybe (Just e)  = Just (eval e)
+    evalMaybe Nothing   = Nothing
--- homework5/Calc/test/LibSpec.hs
+++ homework5/Calc/test/LibSpec.hs
@@ -4,7 +4,7 @@ import Test.Hspec
 --import Test.Hspec.QuickCheck
 
 import ExprT
-import Lib (eval)
+import Lib (eval, evalStr)
 
 main :: IO ()
 main = hspec spec
@@ -14,6 +14,15 @@ spec =
   describe "Lib" $ do
       it "eval example 1" $ do 
         eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+      it "evalStr example 1" $ do 
+        evalStr "(2+3)*4" `shouldBe`
+          Just (eval $ Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+      it "evalStr example 2" $ do 
+        evalStr "2+3*4" `shouldBe`
+          Just (eval $ Add (Lit 2) (Mul (Lit 3) (Lit 4)))
+      it "evalStr example 3" $ do 
+        evalStr "2+3*" `shouldBe`
+          Nothing
   --  it "works" $ do
   --    True `shouldBe` True
   --  prop "ourAdd is commutative" $ \x y ->
