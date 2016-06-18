7c4420e671eac2ed1e28f9ff36f370b3fb36ddb9
David <daveparrish@gmail.com>
2016-06-17 22:04:01 -0400

    Add exercise 5 from homework 5

--- homework5/Calc/app/Main.hs
+++ homework5/Calc/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib (eval)
+--import Lib (eval)
 
 import Text.Printf (printf)
 
--- homework5/Calc/src/Lib.hs
+++ homework5/Calc/src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
 -- | A library to do stuff.
 module Lib
     (
@@ -7,9 +9,11 @@ module Lib
     , testExp
     , MinMax (MinMax)
     , Mod7 (Mod7)
+    , compile
     ) where
 
 import ExprT
+import StackVM
 import Parser (parseExp)
 
 --data ExprT = Lit Integer
@@ -19,13 +23,13 @@ import Parser (parseExp)
 
 -- Exercise 1
 eval :: ExprT -> Integer
-eval (Mul e1 e2)  = (eval e1) * (eval e2)
-eval (Add e1 e2)  = (eval e1) + (eval e2)
-eval (Lit i)      = i
+eval (ExprT.Mul e1 e2)  = (eval e1) * (eval e2)
+eval (ExprT.Add e1 e2)  = (eval e1) + (eval e2)
+eval (ExprT.Lit i)      = i
 
 -- Exercise 2
 evalStr :: String -> Maybe Integer
-evalStr s = evalMaybe $ parseExp Lit Add Mul s
+evalStr s = evalMaybe $ parseExp ExprT.Lit ExprT.Add ExprT.Mul s
   where 
     evalMaybe :: Maybe ExprT -> Maybe Integer
     evalMaybe (Just e)  = Just (eval e)
@@ -39,8 +43,8 @@ class Expr a where
 -- Looked up some help here:
 -- https://github.com/BerndSchwarzenbacher/cis194-solutions/blob/master/05-typeclasses/Calc.hs
 instance Expr ExprT where
-  mul = Mul
-  add = Add
+  mul = ExprT.Mul
+  add = ExprT.Add
   lit = Lit
 
 -- Exercise 4
@@ -68,3 +72,14 @@ instance Expr Mod7 where
   mul (Mod7 x) (Mod7 y) = Mod7 ((x*y) `mod` 7)
   add (Mod7 x) (Mod7 y) = Mod7 ((x+y) `mod` 7)
   lit x   = Mod7 (x `mod` 7)
+
+-- Exercise 5
+-- Used this solution for help:
+-- https://github.com/BerndSchwarzenbacher/cis194-solutions/blob/master/05-typeclasses/Calc.hs
+instance Expr StackVM.Program where
+  lit i   = [StackVM.PushI i]
+  add x y = x ++ y ++ [StackVM.Add]
+  mul x y = x ++ y ++ [StackVM.Mul]
+
+compile :: String -> Maybe Program
+compile s = parseExp lit add mul s
