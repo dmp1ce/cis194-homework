4bd63953bdba97c5d495cb35d53c8639899c0935
David <daveparrish@gmail.com>
2016-06-18 14:18:31 -0400

    Add exercise 3 and part of 4 for homework 6

--- homework6/fib/src/Lib.hs
+++ homework6/fib/src/Lib.hs
@@ -4,6 +4,9 @@ module Lib
       fib
     , fibs1
     , fibs2
+    , Stream (Cons)
+    , streamToList
+    , streamRepeat
     ) where
 
 fib :: Integer -> Integer
@@ -22,3 +25,18 @@ fibs2 = fibgen 0 1 where
   fibgen :: Integer -> Integer -> [Integer]
   fibgen a b = a : fibgen b (a + b) -- Instead of calculating the fib again just add the last two numbers in the squence together
 
+-- Used reference here: https://github.com/evansb/cis194-hw/blob/master/spring_2013/hw6/Fibonacci.hs
+-- Exercise 3
+streamToList :: Stream a -> [a]
+streamToList (Cons x rest) = x:(streamToList rest)
+
+data Stream a = Cons a (Stream a)
+instance Show a => Show (Stream a) where
+--  --show a = loop (20::Int) a where
+  show  = loop (20::Int)  where
+            loop 0  _ = "\n"
+            loop n (Cons x0 rest) = show x0 ++ ", " ++ loop (n - 1) rest
+
+-- Exercise 4
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
