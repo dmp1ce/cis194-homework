e54688200dc82b74d65828ec24c5d85c17c7bb5a
David <daveparrish@gmail.com>
2016-07-03 13:59:00 -0400

    Add exercise 3 of homework 8

--- homework8/Party/src/Party.hs
+++ homework8/Party/src/Party.hs
@@ -4,6 +4,7 @@ module Party
       glCons
     , moreFun
     , treeFold
+    , nextLevel
     ) where
 
 import Data.Tree
@@ -23,3 +24,14 @@ moreFun gl0 gl1
 -- And here: https://github.com/surganov/cis194/blob/master/08/Party.hs
 treeFold :: (a -> [b] -> b) -> Tree a -> b
 treeFold f (Node r xs) = f r (map (treeFold f) xs)
+
+-- Exercise 3
+nextLevel :: Employee -> [(GuestList, GuestList)]
+  -> (GuestList, GuestList)
+nextLevel b (xs) =
+  ((GL [b] (empFun b)) `mappend` (snd totalGuestLists),
+    (fst totalGuestLists))
+  where
+    totalGuestLists = foldr appendTuples (mempty,mempty) xs
+    appendTuples = (\x acc ->
+      ((fst x) `mappend` (fst acc),(snd x) `mappend` (snd acc)) )
--- homework8/Party/test/PartySpec.hs
+++ homework8/Party/test/PartySpec.hs
@@ -5,7 +5,7 @@ import Test.Hspec
 
 import Employee
 import Data.Tree
-import Party (glCons, treeFold)
+import Party (glCons, treeFold, nextLevel)
 
 main :: IO ()
 main = hspec spec
@@ -23,10 +23,35 @@ spec =
       let glBobSteve = GL [steve,bob] 3
       it "Add guest to guest list that bob is on" $ do
         steve `glCons` glOnlyBob `shouldBe` glBobSteve
+
     context "treeFold" $ do
       let myTree = Node "a" [Node "b" [], Node "c" [], Node "d" []]
       it "treeFold works with strings" $ do
         treeFold (\t l -> t ++ concat l) myTree
           `shouldBe` "abcd"
+
+    context "nextLevel" $ do
+      let bob       = Emp "Bob" 1
+      let gl        = GL [bob] (empFun bob)
+      let glNoBoss  = GL [] 0
+      it "Empty sub trees" $ do
+        nextLevel bob [] `shouldBe` (gl, glNoBoss)
+      let dave = Emp "Dave" 1
+      let mell = Emp "Mell" 2
+      let will = Emp "Will" 10
+      let glSub       = GL [dave,will] 11
+      let glSubNoBoss = GL [mell] 2
+      it "One subtree" $ do
+        nextLevel bob [(glSub, glSubNoBoss)] `shouldBe`
+          (GL [bob,mell] 3, GL [dave,will] 11)
+      let marc = Emp "Marc" 2
+      let donna = Emp "Donna" 1
+      let lenna = Emp "Lenna" 5
+      let glSub2          = GL [marc] 2
+      let glSub2NoBoss    = GL [donna, lenna] 6
+      it "Two subtrees" $ do
+        nextLevel bob [(glSub, glSubNoBoss),(glSub2,glSub2NoBoss)] `shouldBe`
+          (GL [bob,mell,donna,lenna] 9, GL [dave,will,marc] 13)
+
 --      prop "ourAdd is commutative" $ \x y ->
 --        ourAdd x y `shouldBe` ourAdd y x
