23445667e568609c85c94f95525141f221b1d807
David <daveparrish@gmail.com>
2016-07-11 18:41:48 -0400

    Add Exercises 3-5 for homework 10

--- homework10/Parser/src/AParser.hs
+++ homework10/Parser/src/AParser.hs
@@ -4,9 +4,8 @@
 
 module AParser where
 
---import           Control.Applicative
-
 import           Data.Char
+import           Control.Applicative
 
 -- A parser for a value of type a is a function which takes a String
 -- represnting the input to be parsed, and succeeds or fails; if it
@@ -72,4 +71,42 @@ instance Functor Parser where
   fmap f (Parser a)    = Parser (fmap (first f) . a)
 
 -- Exercise 2
---instance Applica
+instance Applicative Parser where
+  -- Looked up solution here: https://github.com/evansb/cis194-hw/blob/master/spring_2013/hw10/AParser.hs
+  -- A lamda function of type String -> Maybe (a, String) to match Parser parameter
+  pure a    = Parser (\str -> Just (a, str))
+  -- and here: https://github.com/BerndSchwarzenbacher/cis194-solutions/blob/master/10-applicative-functors-part1/AParser.hs
+  p1 <*> p2 = Parser f
+    where
+      f str = case runParser p1 str of
+                Nothing               -> Nothing
+                Just (m, str')        -> first m <$> runParser p2 str'
+
+-- Exercise 3
+-- Looked up hint here: https://github.com/evansb/cis194-hw/blob/master/spring_2013/hw10/AParser.hs
+-- We can use char function to produce a Parser by passing in a Char. Using Applicative and fmap (<$>) to form a tuple Parser
+abParser :: Parser (Char, Char)
+abParser = (\a b -> (a,b)) <$> char 'a' <*> char 'b'
+
+abParser_ :: Parser ()
+abParser_ = (\_ _ -> ()) <$> char 'a' <*> char 'b'
+
+intPair :: Parser [Integer]
+intPair = (\i1 _ i2 -> [i1,i2]) <$> posInt <*> char ' ' <*> posInt
+
+-- Exercise 4
+-- Again looked up solution here: https://github.com/evansb/cis194-hw/blob/master/spring_2013/hw10/AParser.hs
+-- and then modified a little so I could better understand what was going on
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  a <|> b = Parser pFunc where
+      pFunc str = case runParser a str of
+        Nothing -> runParser b str
+        something -> something
+
+-- Exercise 5
+-- Again looked up solution here: https://github.com/evansb/cis194-hw/blob/master/spring_2013/hw10/AParser.hs#L91
+-- After I had most but got stuck with the multiple fmaps and then an Alternative between fmap results.
+-- I changed the solution a little to better understand, essentually implementing const with lamba function.
+intOrUpperCase :: Parser ()
+intOrUpperCase = (\_ -> ()) <$> posInt <|> (\_ -> ()) <$> satisfy (isUpper)
